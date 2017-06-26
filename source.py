13e60b1ebcffee35194e4c1ea2a4c5726d89c240
Andrey <dips3095@gmail.com>
2017-06-26 17:56:17 +0300

    Translation to English and removing commentaries

--- source.py
+++ source.py
@@ -34,7 +34,7 @@ def check_date(check_folder, match_folder):
                 text1 = " ".join(text1.split("\n"))
                 words1 = tokenize(text1)
                 if set(words) == set(words1):
-                    print('Файл есть в тестовой выборке: ' + str(it) + ' ' + str(jt))
+                    print('File is in testing set: ' + str(it) + ' ' + str(jt))
 
 
 def get_yules(s, ResArray):
@@ -50,7 +50,6 @@ def get_yules(s, ResArray):
     m2 = sum([freq ** 2 for freq in token_counter.values()])
     i = (m1 * m1) / (m2 - m1)
     k = 1 / i * 10000
-    # filename.write("Yule K метрика: " + str(k) + "\n")
     ResArray[6] = k
     return k
 
@@ -67,7 +66,6 @@ def Hapax(s, ResArray):
     for word in freqs:
         if freqs[word] == 1: cnthapax += 1
 
-    # filename.write("Hapax: " + str(cnthapax / cnttokens) + "\n")
     ResArray[5] = cnthapax / cnttokens
 
 
@@ -84,12 +82,9 @@ def safeSplit(regex, text):
 
 def AvgSentLength(sentences):
     cnt = len(sentences) - 1
-    # print(cnt, "\n")
-    res = 0.0
     avg = 0.0
 
     for sent in sentences:
-        # print(sent, "\n------------")
         words = sent.split()
         avg += len(words)
     return (avg / cnt)
@@ -112,7 +107,6 @@ def LexicalCharacterFeature(text, ResArray):
     lettermap = {}
     specsymbolmap = dict.fromkeys(Specsymbols, 0)
     punctuationmap = dict.fromkeys(Punctuations, 0)
-
     while i != len(text) - 1:
         if text[i].isdigit(): countDigits += 1
         if text[i].isalpha():
@@ -130,27 +124,11 @@ def LexicalCharacterFeature(text, ResArray):
         if text[i] in Punctuations:
             countPunctuations += 1
             punctuationmap[text[i]] += 1
-
         i += 1
         ResArray[7] = countLetters / len(text)
-        # ResArray[8] = countDigits / len(text)
         ResArray[8] = countPunctuations / len(text)
         ResArray[9] = countSpecsymbols / len(text)
         ResArray[10] = countSpaces / len(text)
-        # filename.write('Общее число символов(N): ' + str(len(text)) + "\n")
-        # filename.write('Количество цифр/N: ' + str(countDigits / len(text)) + "\n")
-        # filename.write('Количество букв/N: ' + str(countLetters / len(text)) + "\n")
-        # filename.write('Количество заглавных букв/N: ' + str(countUppercaseLetters / len(text)) + "\n")
-        # filename.write('Количество пробелов/N: ' + str(countSpaces / len(text)) + "\n")
-        # filename.write('Количество табуляций/N: ' + str(countTabs / len(text)) + "\n")
-        # filename.write('Количество спецсимволов/N: ' + str(countSpecsymbols / len(text)) + "\n")
-        # filename.write('Количество пунктуационных символов/N: ' + str(countPunctuations / len(text)) + "\n")
-        # filename.write('Счетчик букв:\n')
-        # sortdict = lettermap.keys()
-        # sortdict = list(sortdict)
-        # sortdict.sort()
-        # for letter in sortdict:
-        #     filename.write(letter + ' ' + str((lettermap[letter])) + "\n")
 
 
 def LexicalWordBasedFeature(sentences, ResArray):
@@ -161,8 +139,6 @@ def LexicalWordBasedFeature(sentences, ResArray):
     cntShortWords = 0.0
     cntLongWodrs = 0.0
     cntProperNames = 0.0
-    firstWordinSentence = 1
-
     for sent in sentences:
         cntLenSentences += len(sent)
         words = tokenize(sent)
@@ -171,22 +147,15 @@ def LexicalWordBasedFeature(sentences, ResArray):
         for word in words:
             cntWordLenth += len(word)
             if len(word) < 4: cntShortWords += 1
-            if firstWordinSentence == 0 and word.istitle(): cntProperNames += 1
-            firstWordinSentence = 0
+            if firstWordinSentence == 1 and word.istitle():
+                cntProperNames += 1
+                firstWordinSentence = 0
 
     avgWordLength = cntWordLenth / cntWords
-    # filename.write("Общее количество слов(T): " + str(cntWords) + "\n")
-    # filename.write("Общее количество предложений): " + str(cntSentences) + "\n")
-    # filename.write("Средняя длина предложений(в символах):" + str(cntLenSentences / cntSentences) + "\n")
-    # filename.write("Средняя длина слов: " + str(avgWordLength) + "\n")
-    # filename.write("Короткие слова(1-3 символа)/T: " + str(cntShortWords / cntWords) + "\n")
     for sent in sentences:
         words = sent.split()
         for word in words:
             if len(word) >= avgWordLength: cntLongWodrs += 1
-
-    # filename.write("Длинные слова(слова с длиной больше средней)/T: " + str(cntLongWodrs / cntWords) + "\n")
-    # filename.write("Кооличество имен собственных/T: " + str(cntProperNames / cntWords) + "\n")
     ResArray[0] = cntLenSentences / cntSentences
     ResArray[1] = avgWordLength
     ResArray[2] = cntShortWords / cntWords
@@ -209,53 +178,38 @@ re1 = re.compile("""
 
 
 def f1(Res):
-    # old 15 files return -0.66531213 * Res[1] - 1.74932097 * Res[3] + 1.55590547 * Res[5] + 0.01443481 * Res[6] + 2.5513533
-    # old 30 files return 3.87344423 * Res[2] + 2.09653228 * Res[4] + 0.00931351 * Res[6] - 2.12646138
     return 0.00114659 * Res[0] - 1.11005179 * Res[1] - 1.52572186 * Res[3] - 0.79935266 * Res[5] + 0.01124218 * Res[
         6] + 53.43603776 * Res[9] - 48.3560366 * Res[10] + 13.28955791
 
 
 def f2(Res):
-    # olf return -0.37162769 * Res[1] - 2.15999379 * Res[3] + 4.51894416 * Res[5] + 0.00976478 * Res[6] + 0.17700341
-    # old 30 files return 2.71987327 * Res[2] - 1.60477892 * Res[3] + 2.88512435 * Res[4] + 3.09086243 * Res[5] + 0.00976104 * Res[6] - 2.49476899
     return -0.41670645 * Res[1] + 1.10474101 * Res[2] - 1.80510707 * Res[3] + 1.49446929 * Res[4] + 2.53937585 * Res[
         5] + 0.01083192 * Res[6] - 6.95972671 * Res[7] - 33.55335051 * Res[9] - 27.19890629 * Res[10] + 10.59148444
 
 
 def f3(Res):
-    # old return 0.0061358 * Res[0] - 0.89109292 * Res[1] + 3.34285494 * Res[4] + 0.01302892 * Res[6] + 2.88912527
-    # return 0.00259444 * Res[0] - 0.59364292 * Res[1] - 2.88400263 * Res[2] - 2.40832936 * Res[3] - 0.98683601 * Res[5] + 0.01126026 * Res[6] + 5.32713185
     return 0.00334412 * Res[0] - 0.80241887 * Res[1] - 3.08843481 * Res[2] - 2.70885979 * Res[3] - 0.89942445 * Res[
         5] + 0.01230323 * Res[6] + 15.42688185 * Res[7] + 23.25931525 * Res[8] + 43.3122369 * Res[9] - 6.88875441
 
 
 def f4(Res):
-    # old return 0.0084909 * Res[0] - 0.84377454 * Res[1] + 3.81141367 * Res[4] + 3.99526148 * Res[5] + 0.00803095 * Res[6] + 0.56127883
-    # return 0.0035173 * Res[0] - 0.43246268 * Res[1] - 1.95920243 * Res[2] - 2.77518217 * Res[3] + 2.64232624 * Res[4] + 2.28100837 * Res[5] + 0.01164255 * Res[6] + 2.29302797
     return 0.00305075 * Res[0] - 2.49774282 * Res[3] + 2.63514397 * Res[4] + 2.36636696 * Res[5] + 0.01122454 * Res[
         6] + 11.48356245 * Res[7] + 28.32561341 * Res[8] - 38.53047966 * Res[9] + 20.24281479 * Res[10] + -13.7654428
 
 
 def f5(Res):
-    # return -0.00487343 * Res[0] - 2.5557295 * Res[5] + 2.55706235
     return -0.00181438 * Res[0] - 1.12130947 * Res[1] - 2.32413906 * Res[4] - 3.27167373 * Res[5] - 12.35495529 * Res[
         7] - 33.08816484 * Res[8] + 91.17542537 * Res[9] - 71.12103751 * Res[10] + 29.86021335
 
-    # old  return -0.00610943 * Res[0] + 3.05443594 * Res[2] - 2.86487486 * Res[5] + 1.59262578
-
 
 def f6(Res):
-    # return -0.00379366 * Res[0] + 0.3698661 * Res[1] + 3.75422388 * Res[2] + 1.06826961 * Res[3] + 0.90457188 * Res[5] - 3.16389093
     return -0.00240753 * Res[0] - 0.22231009 * Res[1] + 3.47015636 * Res[2] + 1.33443051 * Res[3] - 17.74857373 * Res[
         7] - 27.55552932 * Res[8] - 49.51993133 * Res[10] + 22.77590225
 
-    # old  return -0.00653362 * Res[0] + 0.62713759 * Res[1] + 3.86184454 * Res[2] - 3.04419763
 
 
 def f7(Res):
     return -3.6218862 * Res[5] + 76.37126 * Res[9] + -8.24426171 * Res[10] + 3.95940794
-    # return -3.26575849 * Res[5] + 2.66313208
-    # old return 2.56711228 * Res[2] - 3.50941948 * Res[5] + 1.74198514
 
 
 def f1_mr5(Res):
@@ -269,14 +223,6 @@ def f1_mr5(Res):
     if my_cmp(Res[0], 53.361, 1e-3, op.le):
         return 0.051 * Res[0] - 2.2661
     return 1
-    # swap 1 abd 3
-    # if my_cmp(Res[0], 103.203, 1e-3, op.le):
-    #     return 0.0007 * Res[0] + 1.691 * Res[10] - 0.3112
-    # if Res[0] > 143.198:
-    #     return 0.0009 * Res[0] + 15.6087 * Res[10] - 1.7429
-    # if my_cmp(Res[10], 0.161, 1e-3, op.le):
-    #     return 58.7483 * Res[10] - 9.037
-    # return 1
 
 
 def f2_mr5(Res):
@@ -294,16 +240,6 @@ def f2_mr5(Res):
         return -0.0171 * Res[0] + 2.1992
     return -0.0666 * Res[0] + 6.4226
 
-    # swap 1 and 3 class
-    # if my_cmp(Res[0], 143.198, 1e-3, op.le) and Res[5] > 0.613:
-    #     return 0.0006 * Res[0] + 3.3781 * Res[5] - 112.9966 * Res[9] - 1.4452
-    # if my_cmp(Res[0], 143.198, 1e-3, op.le) and Res[5] > 0.522:
-    #     return 0.0015 * Res[0] - 0.3313 * Res[2] - 1.3677 * Res[5] + 0.8036
-    # if Res[0] > 143.198:
-    #     return 0.0005 * Res[0] - 1.6226 * Res[2] + 1.4543
-    # if Res[2] > 0.416:
-    #     return 0.0053 * Res[0] - 0.2839
-    # return 1
 
 
 def f3_mr5(Res):
@@ -353,20 +289,6 @@ def f5_mr5(Res):
     if Res[2] > 0.416:
         return -0.0053 * Res[0] + 1.2839
     return 0
-    # swap 1 and 3
-    # if Res[0] > 103.737:
-    #     return 0.0003 * Res[0] - 0.9715 * Res[5] - 0.0036 * Res[6] - 7.8353 * Res[8] + 1.8106
-    # if Res[8] > 0.031:
-    #     return 0.0035 * Res[0] - 0.9583 * Res[5] - 0.0042 * Res[6] + 0.6683
-    # if my_cmp(Res[6], 60.319, 1e-3, op.le) and my_cmp(Res[5], 0.568, 1e-3, op.le):
-    #     return 0.0102 * Res[0] - 1.6117 * Res[5] - 0.004 * Res[6] + 9.9906 * Res[8] + 7.2324 * Res[10] - 0.3125
-    # if Res[6] > 50.73:
-    #     return 0.005 * Res[0] - 0.8481 * Res[5] - 0.006 * Res[6] + 0.6048
-    # if my_cmp(Res[5], 0.708, 1e-3, op.le) and my_cmp(Res[7], 0.809, 1e-3, op.le):
-    #     return 0.0088 * Res[0] - 1.5346 * Res[5] - 8.3927 * Res[7] + 7.8305
-    # if my_cmp(Res[0], 81.569, 1e-3, op.le):
-    #     return 0.0171 * Res[0] - 1.1992
-    # return 0.0666 * Res[0] - 5.4226
 
 
 def f6_mr5(Res):
@@ -377,17 +299,6 @@ def f6_mr5(Res):
     if my_cmp(Res[10], 0.161, 1e-3, op.le):
         return -58.7483 * Res[10] + 10.037
     return 0
-    # swap 1 and 3
-    # if my_cmp(Res[1], 4.76, 1e-2, op.le) and Res[6] > 49.272 and my_cmp(Res[0], 107.298, 1e-3, op.le):
-    #     return 0.0005 * Res[0] + 0.0724 * Res[1] - 1.3805 * Res[2] - 0.0109 * Res[6] - 4.7121 * Res[7] - 93.8703 * Res[
-    #         9] + 5.4647
-    # if Res[0] > 72.158:
-    #     return 1
-    # if Res[0] > 63.332:
-    #     return 1
-    # if my_cmp(Res[0], 53.361, 1e-3, op.le):
-    #     -0.051 * Res[0] + 3.2661
-    # return 0
 
 
 def f7_mr5(Res):
@@ -420,7 +331,6 @@ def makevec(Res):
     return res
 
 
-
 def getArray(src):
     sys = open(src, 'r')
     text = "".join(sys.readlines())
@@ -432,7 +342,7 @@ def getArray(src):
     Hapax(text, ResArray)
     get_yules(text, ResArray)
     return makevec(ResArray)
-    # return makevec_mr5(ResArray)
+    # return makevec_mr5(ResArray) Choose it if you want classifications functions by M5-rules
 
 
 def out_file(src, out):
@@ -506,6 +416,7 @@ def classify_folder_mild(folder, list_of_classifiers):
     cntSuccess = 0
     cntFail = 0
     cntAmongSuccess = 0
+    cntMisClasses = []
     classid = int(folder[-2])
 
     for file in files:
@@ -516,8 +427,12 @@ def classify_folder_mild(folder, list_of_classifiers):
             else:
                 cntFail += 1
         for i in class_list:
-            if i == classid: cntAmongSuccess += 1
+            if i == classid:
+                cntAmongSuccess += 1
+                cntMisClasses.append(len(class_list))
+
     resvec = np.array([len(files), cntSuccess, cntFail, cntAmongSuccess])
+
     return resvec
 
 
@@ -527,19 +442,19 @@ def print_res_all(seq):
     for i in seq:
         res_strong += classify_folder_strong(i, list_of_classifiers)
         res_mild += classify_folder_mild(i, list_of_classifiers)
-    print('Жесткое декодирование')
-    print('Всего было обработано {} файла, из них:'.format(int(res_strong[0])))
-    print('Верно определен класс: ' + str(int(res_strong[1])))
-    print('Из верно определенных было исправление: {}'.format(int(res_strong[4])))
-    print('Класс определен, но не верно: ' + str(int(res_strong[2])))
-    print('Не декодируется ни в один из классов: ' + str(int(res_strong[3])))
+    print('Hard decoding')
+    print('{} files have been processed and the result is:'.format(int(res_strong[0])))
+    print('Successfully classified: {}'.format(int(res_strong[1])))
+    print('Among them, classified using error-correcting: {}'.format(int(res_strong[4])))
+    print('Misclassified: {}'.format(int(res_strong[2])))
+    print('Cannot be decoded in any class : {}'.format(int(res_strong[3])))
     print()
-    print('Мягкое декодирование')
-    print('Всего было обработано {} файла, из них:'.format(int(res_mild[0])))
-    print('Верно однозначно определен класс: ' + str(int(res_mild[1])))
-    print('Из верно определенных было исправление: {}'.format(int(res_strong[4])))
-    print('Неверно определен класс: ' + str(int(res_mild[2])))
-    print('Класс определен верно, но неоднозначно: ' + str(int(res_mild[3])))
+    print('Soft decoding')
+    print('{} files have been processed and the result is:'.format(int(res_mild[0])))
+    print('Successfully classified: {}'.format(int(res_mild[1])))
+    print('Among them, classified using error-correcting: {}'.format(int(res_strong[4])))
+    print('Misclassified: {}'.format(int(res_mild[2])))
+    print('Ambiguous classification'.format(int(res_mild[3])))
 
 
 trainingset1 = 'dataset/diminC1/'
@@ -552,19 +467,5 @@ testset3 = 'test_data/data_for_testC3/'
 testset4 = 'test_data/data_for_testC4/'
 list_of_classifiers = [[1, 1, 1, 1, 1, 1, 1], [0, 0, 0, 0, 1, 1, 1], [0, 0, 1, 1, 0, 0, 1], [0, 1, 0, 1, 0, 1, 0]]
 
-# classify_folder_strong(testset1, list_of_classifiers)
-# print(classify_folder_mild(testset1, list_of_classifiers))
-print_res_all([testset2, testset1, testset4, testset3])
-# out_file('dataset/diminC1/', 0)
-# out_file('dataset/diminC1/', 1)
-# out_file('dataset/Tyler-84C2/', 0)
-# out_file('dataset/Tyler-84C2/', 1)
-# out_file('dataset/VadimCzechC3/', 0)
-# out_file('dataset/VadimCzechC3/', 1)
-# out_file('dataset/LandolkaC4/', 0)
-# out_file('dataset/LandolkaC4/', 1)
-# check_date('data_for_testC3/','data_for_testC3/')
-# print('------')
-# check_date(testset4,trainingset4)
-# print('------')
-# check_date(testset4,testset4)
+# print_res_all([trainingset1, trainingset2, trainingset3, trainingset4])
+print_res_all([testset1, testset2, testset3, testset4])
